9344e336463f7eea507783c32fc26cea20c9da1f
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-01 11:55:01 +0100

    Convert Function/method added to the app

--- New Android/Unit Coverter App/app/src/main/java/com/example/unitconverterapp/MainActivity.kt
+++ New Android/Unit Coverter App/app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.unitconverterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.editText8)
+        val btn = findViewById<Button>(R.id.btn)
+        val textView = findViewById<TextView>(R.id.result_text)
+
+        btn.setOnClickListener() {
+            val kilos: Double = editText.text.toString().toDouble()
+            textView.setText("" +convertToPounds(kilos)+ "\n Pounds")
+
+        }
+
+    }
+
+    fun convertToPounds(kilos : Double) : Double {
+        var pounds = kilos * 2.20462
+        return pounds
     }
 }
