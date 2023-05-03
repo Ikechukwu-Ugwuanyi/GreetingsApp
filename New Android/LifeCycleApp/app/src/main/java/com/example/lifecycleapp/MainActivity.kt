bcb0e04cdb7b3e59696124fc2ae3f1aef0f8f750
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-03 11:48:49 +0100

    Onstart and OnResume methods implemented.

--- New Android/LifeCycleApp/app/src/main/java/com/example/lifecycleapp/MainActivity.kt
+++ New Android/LifeCycleApp/app/src/main/java/com/example/lifecycleapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.lifecycleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "onCreate() is called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Toast.makeText(this, "onStart() is called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume() is called", Toast.LENGTH_SHORT).show()
     }
 }
