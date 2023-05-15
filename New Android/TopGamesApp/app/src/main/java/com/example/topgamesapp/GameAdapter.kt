be13c2690fd1cf2b4842d0811981d7571a577605
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-15 11:20:01 +0100

    Adapter Code updated.

--- New Android/TopGamesApp/app/src/main/java/com/example/topgamesapp/GameAdapter.kt
+++ New Android/TopGamesApp/app/src/main/java/com/example/topgamesapp/GameAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class GameAdapter(val gamesList: ArrayList<GameModel>) :
@@ -17,9 +18,14 @@ class GameAdapter(val gamesList: ArrayList<GameModel>) :
             init {
                 gameImg = itemView.findViewById(R.id.cardviewImg)
                 gameTitle = itemView.findViewById(R.id.cardViewTxt)
+
+                itemView.setOnClickListener() {
+                    Toast.makeText(itemView.context, "You Chose: ${gamesList[adapterPosition].cardTxt}",
+                    Toast.LENGTH_SHORT)
+                }
             }
 
-           // itemView.setOnclickListener() {}
+
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
