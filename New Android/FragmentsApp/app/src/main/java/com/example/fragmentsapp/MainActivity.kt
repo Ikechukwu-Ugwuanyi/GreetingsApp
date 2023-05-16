ebf972cfe5502bbe94100ffb4359e267ea1fa63e
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-16 10:41:36 +0100

    Main Activity code updated. Fragments created!

--- New Android/FragmentsApp/app/src/main/java/com/example/fragmentsapp/MainActivity.kt
+++ New Android/FragmentsApp/app/src/main/java/com/example/fragmentsapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.fragmentsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1: Button = findViewById(R.id.btn1)
+        val btn2: Button = findViewById(R.id.btn2)
+
+        btn1.setOnClickListener() {
+            val fragment1: Fragment = LoginFragment()
+            val fragment2: Fragment = Fragment2()
+
+            val ftrans: FragmentTransaction = supportFragmentManager.beginTransaction()
+            ftrans.replace(R.id.frameLayout1, fragment1)
+                .commit()
+        }
     }
 }
