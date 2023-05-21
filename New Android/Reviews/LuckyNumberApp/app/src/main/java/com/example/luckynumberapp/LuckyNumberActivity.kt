22391b4635d12c5735caa9282bdfedd4b849f994
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-21 07:19:17 +0100

    New files and code added.

--- New Android/Reviews/LuckyNumberApp/app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.kt
+++ New Android/Reviews/LuckyNumberApp/app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.kt
@@ -1,11 +1,46 @@
 package com.example.luckynumberapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.Random
 
 class LuckyNumberActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lucky_number)
+
+        val txtTitle : TextView = findViewById(R.id.textView2)
+        val luckyNum : TextView = findViewById(R.id.luckyTxt)
+
+        val btnShare: Button = findViewById(R.id.btnShare)
+
+        var user_name = receiveUsername()
+
+        var random_num = generateRandomNum()
+
+        luckyNum.setText(""+random_num)
+    }
+
+    fun receiveUsername(): String {
+        val bundle: Bundle? = intent.extras
+        val userName = bundle?.get("name").toString()
+
+        return userName
+    }
+
+    //Random Numbers Generator
+    fun generateRandomNum(): Random {
+        val random = Random(1000)
+        return random
+    }
+
+    //Sharing
+    fun shareData(username: String, num: Int) {
+        var i = Intent(Intent.ACTION_SEND)
+        i.putExtra(Intent.EXTRA_SUBJECT, "$username is lucky today!")
+        i.putExtra(Intent.EXTRA_TEXT, "His Lucky number is $num")
     }
 }
--- New Android/Reviews/LuckyNumberApp/app/src/main/java/com/example/luckynumberapp/MainActivity.kt
+++ New Android/Reviews/LuckyNumberApp/app/src/main/java/com/example/luckynumberapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.luckynumberapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text: TextView = findViewById(R.id.textView)
+        val editText: TextView = findViewById(R.id.editText)
+        val btnWish : Button = findViewById(R.id.btnWish)
+
+        btnWish.setOnClickListener() {
+            val name = editText.text
+            val intent: Intent = Intent(this, LuckyNumberActivity::class.java)
+            intent.putExtra("name", name)
+
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
