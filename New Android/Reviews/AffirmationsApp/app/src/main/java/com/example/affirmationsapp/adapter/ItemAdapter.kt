589e21fdad2a9dae385e04efff670ac160a5247f
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-22 14:28:42 +0100

    Code Updated

--- New Android/Reviews/AffirmationsApp/app/src/main/java/com/example/affirmationsapp/adapter/ItemAdapter.kt
+++ New Android/Reviews/AffirmationsApp/app/src/main/java/com/example/affirmationsapp/adapter/ItemAdapter.kt
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmationsapp.R
 import com.example.affirmationsapp.model.Affirmation
 
-class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>
+/* class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
@@ -35,4 +35,31 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
         holder.textView.text = context.resources.getString(item.stringResourceId)
         holder.imageView.setImageResource(item.imageResourceId)
     }
+}
+
+ */
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        var textView: TextView = view.findViewById(R.id.item_title)
+        var imgView: ImageView = view.findViewById(R.id.item_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
