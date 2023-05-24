d8ecf0a69b5971566ec5629f8a5a0d44cc89ea0c
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-24 07:17:32 +0100

    MainActivity code updated.

--- New Android/TabLayoutApp/app/src/main/java/com/example/tablayoutapp/Adapter/MyAdapter.kt
+++ New Android/TabLayoutApp/app/src/main/java/com/example/tablayoutapp/Adapter/MyAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.tablayoutapp.Adapter
 
-class MyAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.tablayoutapp.Fragments.BridgeFragment
+import com.example.tablayoutapp.Fragments.PictureFragment
+import com.example.tablayoutapp.Fragments.SeaFragment
+
+class MyAdapter(fragmentManager: FragmentManager, lifeCycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifeCycle) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return SeaFragment()
+
+
+            1 -> return PictureFragment()
+
+
+            2 -> return BridgeFragment()
+        }
+
+        return SeaFragment()
+    }
 }
--- New Android/TabLayoutApp/app/src/main/java/com/example/tablayoutapp/MainActivity.kt
+++ New Android/TabLayoutApp/app/src/main/java/com/example/tablayoutapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.tablayoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.tablayoutapp.Adapter.MyAdapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    val tabsArray = arrayOf("Sea", "Picture","Bridge")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val tabLayout = findViewById<TabLayout>(R.id.tabView)
+
+        val adapter = MyAdapter(supportFragmentManager, lifecycle)
+
+        viewPager.adapter = adapter
     }
 }
