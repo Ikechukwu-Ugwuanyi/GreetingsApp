f76e68cc812c7ab1e3206923c7dde72f73c3efa4
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-25 07:05:18 +0100

    Code added to MainActivity

--- New Android/SharedPreferencesApp/app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
+++ New Android/SharedPreferencesApp/app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.sharedpreferencesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.editText)
+        val btn: Button = findViewById(R.id.saveBtn)
+
+        btn.setOnClickListener() {
+            val enteredText : String = editText.text.toString()
+        }
     }
 }
