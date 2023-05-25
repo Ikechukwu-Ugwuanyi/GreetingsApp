87562901501240bedcaea04fd825e7e2063a928c
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-25 23:31:50 +0100

    MainActivity code updated!

--- New Android/SharedPreferencesApp/app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
+++ New Android/SharedPreferencesApp/app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sharedpreferencesapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -15,6 +16,24 @@ class MainActivity : AppCompatActivity() {
 
         btn.setOnClickListener() {
             val enteredText : String = editText.text.toString()
+            SaveNameInSharedPref(enteredText)
         }
+    }
+
+    private fun SaveNameInSharedPref(enteredText: String) {
+        //Saving the text into Shared Preferences
+        val sharedPreferences : SharedPreferences =
+            getSharedPreferences("UserName", MODE_PRIVATE)
+
+        //Writing data to shared Preferences
+        val editor: SharedPreferences.Editor =
+            sharedPreferences.edit()
+
+        editor.putString("name", enteredText)
+        editor.commit()
+
+
+
+
     }
 }
