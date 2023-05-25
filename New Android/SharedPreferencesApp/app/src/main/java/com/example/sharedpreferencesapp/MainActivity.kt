ac1b6830b3b2a6751a8696f52c5ec05d64183185
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-26 00:14:05 +0100

    MainActivity code updated!

--- New Android/SharedPreferencesApp/app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
+++ New Android/SharedPreferencesApp/app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var nameTextView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val editText: EditText = findViewById(R.id.editText)
+        nameTextView = findViewById(R.id.textView2)
         val btn: Button = findViewById(R.id.saveBtn)
+
+        DisplaySavedName()
 
         btn.setOnClickListener() {
             val enteredText : String = editText.text.toString()
@@ -32,8 +38,16 @@ class MainActivity : AppCompatActivity() {
         editor.putString("name", enteredText)
         editor.commit()
 
+    }
 
+    fun DisplaySavedName() {
+        //Reading data from sharedPreferences
+        val sharedPreferences : SharedPreferences =
+            getSharedPreferences("UserName", MODE_PRIVATE)
 
+        val s1 : String? = sharedPreferences.getString("name", "")
+
+        nameTextView.setText(s1)
 
     }
 }
