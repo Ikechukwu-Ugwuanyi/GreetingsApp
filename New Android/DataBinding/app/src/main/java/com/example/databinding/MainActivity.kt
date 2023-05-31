b75b70165a950a08f0b9167047f5dfb3401ef04d
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-05-31 09:46:20 +0100

    Layout and MainActivity code updated!

--- New Android/DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
+++ New Android/DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        val editText:EditText = findViewById(R.id.editText)
+        val btn:Button = findViewById(R.id.btn)
+
+        btn.setOnClickListener() {
+            val enteredText: String = editText.text.toString()
+            textView.setText("Hello $enteredText")
+        }
+
     }
 }
