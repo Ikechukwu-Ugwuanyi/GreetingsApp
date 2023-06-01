7df58891cfebe8b38d34ac11caba9a58ce491cb1
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-01 06:17:30 +0100

    Code added to layout and MainActivity.

--- New Android/ViewModel/app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ New Android/ViewModel/app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.viewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.textView.text = counter.toString()
+
+        binding.btn.setOnClickListener() {
+            binding.textView.text.toString()
+        }
     }
 }
