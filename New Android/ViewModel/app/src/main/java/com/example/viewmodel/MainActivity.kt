f79a61a3dd212f99b8523af1b64224bd45377d13
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-01 06:51:16 +0100

    MainActivity and VieModel class updated with code!

--- New Android/ViewModel/app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ New Android/ViewModel/app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -3,22 +3,27 @@ package com.example.viewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var counter = 0
+    lateinit var viewModel: MainActivityViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        binding.textView.text = counter.toString()
+
+        binding.textView.text = viewModel.getCurrentCount().toString()
 
         binding.btn.setOnClickListener() {
-            binding.textView.text.toString()
+            binding.textView.text = viewModel.getUpdatedCount().toString()
         }
     }
 }
--- New Android/ViewModel/app/src/main/java/com/example/viewmodel/MainActivityViewModel.kt
+++ New Android/ViewModel/app/src/main/java/com/example/viewmodel/MainActivityViewModel.kt
@@ -3,4 +3,15 @@ package com.example.viewmodel
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
+
+    private var counter = 0
+
+    fun getCurrentCount (): Int  {
+        return counter
+    }
+
+    fun getUpdatedCount (): Int {
+        return ++counter
+    }
+
 }
