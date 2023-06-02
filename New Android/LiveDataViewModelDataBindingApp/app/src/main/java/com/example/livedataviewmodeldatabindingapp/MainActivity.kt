ddb51845b1ab20aa0e0da9dd1803fe08f4bd3f39
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-02 17:27:18 +0100

    Code updated in files!

--- New Android/LiveDataViewModelDataBindingApp/app/src/main/java/com/example/livedataviewmodeldatabindingapp/MainActivity.kt
+++ New Android/LiveDataViewModelDataBindingApp/app/src/main/java/com/example/livedataviewmodeldatabindingapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.livedataviewmodeldatabindingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.livedataviewmodeldatabindingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainActivityViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.myViewModel = viewModel
+
+        viewModel.counter.observe(this) {
+            binding.textView.text = it.toString()
+        }
     }
 }
