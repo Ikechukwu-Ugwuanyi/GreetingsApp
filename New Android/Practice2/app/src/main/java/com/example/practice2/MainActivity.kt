5a6457357fffe7e1f6716dc81d806164e127b74f
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-05 16:26:53 +0100

    MainActivity Code and Fragment3 Updated!

--- New Android/Practice2/app/src/main/java/com/example/practice2/MainActivity.kt
+++ New Android/Practice2/app/src/main/java/com/example/practice2/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.practice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var adapter: MyAdapter
+   lateinit var viewPager2: ViewPager2
+   private val myArray = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager2 = findViewById(R.id.viewPager)
+        adapter = MyAdapter(this)
+
+        viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        viewPager2.adapter = adapter
+
+
+
+
+
     }
+
+
 }
