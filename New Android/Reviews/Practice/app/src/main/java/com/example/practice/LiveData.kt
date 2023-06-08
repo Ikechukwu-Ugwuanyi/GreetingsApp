a3f9d80ef789c640d96e9a91b0fa0a259942825f
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-08 15:10:33 +0100

    Files Updated.

--- New Android/Reviews/Practice/app/src/main/java/com/example/practice/LiveData.kt
+++ New Android/Reviews/Practice/app/src/main/java/com/example/practice/LiveData.kt
@@ -2,10 +2,24 @@ package com.example.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.practice.databinding.ActivityLiveDataBinding
 
 class LiveData : AppCompatActivity() {
+
+    lateinit var binding: ActivityLiveDataBinding
+    lateinit var viewModel: LiveDataViewModel
+    lateinit var factory: LiveDataFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_live_data)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_live_data)
+
+        factory = LiveDataFactory(200)
+
+        viewModel = ViewModelProvider(this, factory).get(LiveDataViewModel::class.java)
     }
 }
--- New Android/Reviews/Practice/app/src/main/java/com/example/practice/LiveDataFactory.kt
+++ New Android/Reviews/Practice/app/src/main/java/com/example/practice/LiveDataFactory.kt
@@ -1,4 +1,16 @@
 package com.example.practice
 
-class LiveDataFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class LiveDataFactory(private val startingNum: Int) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(LiveDataViewModel::class.java)) {
+            return LiveDataViewModel(startingNum) as T
+        }
+
+        throw  IllegalArgumentException("Unknown class")
+    }
+
 }
