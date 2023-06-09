bc1d8a434a6dede0815454bcbf20283435428980
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-09 09:42:33 +0100

    New files created and some code added!

--- New Android/DBVLD/app/src/main/java/com/example/dbvld/MainActivity.kt
+++ New Android/DBVLD/app/src/main/java/com/example/dbvld/MainActivity.kt
@@ -1,28 +1,23 @@
-package com.example.livedataviewmodeldatabindingapp
+package com.example.dbvld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import com.example.livedataviewmodeldatabindingapp.databinding.ActivityMainBinding
+import com.example.dbvld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    lateinit var viewModel: MainActivityViewModel
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var  viewModel : MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        binding.lifecycleOwner = this
-
-        binding.myViewModel = viewModel
 
     }
 }
--- New Android/DBVLD/app/src/main/java/com/example/dbvld/MainActivityViewModel.kt
+++ New Android/DBVLD/app/src/main/java/com/example/dbvld/MainActivityViewModel.kt
@@ -1,14 +1,11 @@
-package com.example.livedataviewmodeldatabindingapp
+package com.example.dbvld
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
 
     private var counter = MutableLiveData<Int>()
-    val countData: LiveData<Int>
-    get() = counter
 
     init {
         counter.value = 0
