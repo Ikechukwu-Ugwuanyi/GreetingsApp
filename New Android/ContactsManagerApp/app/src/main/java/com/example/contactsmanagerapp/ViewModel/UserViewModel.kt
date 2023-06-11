5f40098ebc41ec1cb49a69bf33e6cb94b30e7694
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-11 18:18:56 +0100

    Code updated in files!

---
+++ New Android/ContactsManagerApp/app/src/main/java/com/example/contactsmanagerapp/ViewModel/UserViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.contactsmanagerapp.ViewModel
+
+import android.database.Observable
+import android.provider.SyncStateContract.Helpers.insert
+import androidx.databinding.Bindable
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.contactsmanagerapp.room.User
+import com.example.contactsmanagerapp.room.UserRepository
+import kotlinx.coroutines.launch
+
+class UserViewModel(private val repository: UserRepository) : ViewModel(), Observable {
+
+    val users = repository.users
+    private var isUpdateOrDelete = false
+    private lateinit var userToUpdateOrDelete: User
+
+    @Bindable
+    val  inputName = MutableLiveData<String>()
+
+    @Bindable
+    val  inputEmail = MutableLiveData<String>()
+
+    @Bindable
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+
+    @Bindable
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
+    }
+
+    fun saveOrUpdate() {
+        val name = inputName.value!!
+        val email = inputEmail.value!!
+
+        insert(User(0, name, email))
+
+        inputName.value = null
+        inputEmail.value = null
+    }
+
+    fun clearAllOrDelete() {
+        clearAll()
+    }
+
+
+
+    fun insert(user : User) = viewModelScope.launch {
+        repository.insert(user)
+    }
+
+    fun clearAll() = viewModelScope.launch {
+        repository.deleteAll()
+    }
+}
--- New Android/ContactsManagerApp/app/src/main/java/com/example/contactsmanagerapp/room/User.kt
+++ New Android/ContactsManagerApp/app/src/main/java/com/example/contactsmanagerapp/room/User.kt
@@ -10,7 +10,7 @@ data class User(
     @ColumnInfo(name = "user_id")
     val id: Int,
 
-    @ColumnInfo(name = "user_id")
+    @ColumnInfo(name = "user_name")
     val name: String,
 
     @ColumnInfo(name = "user_email")
