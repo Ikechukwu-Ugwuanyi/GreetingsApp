8a80ce4238e0273f12d62584dea0b71d19e5cb19
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-13 14:30:22 +0100

    Code updated in files!

---
+++ New Android/NavApp/app/src/main/java/com/example/navapp/SecondFragment.kt
@@ -0,0 +1,28 @@
+package com.example.navapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navapp.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+   lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_second, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
+    }
+
+
+
+}
