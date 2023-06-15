c66f1cac46df0264448e285969caef453d732b33
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-15 15:21:36 +0100

    File structure changed. Code added to the Entity class, Note

--- New Android/NoteTakingApp/app/src/main/java/com/example/notetakingapp/fragments/MainActivity.kt
+++ New Android/NoteTakingApp/app/src/main/java/com/example/notetakingapp/fragments/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.notetakingapp
+package com.example.notetakingapp.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notetakingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ New Android/NoteTakingApp/app/src/main/java/com/example/notetakingapp/model/Note.kt
@@ -0,0 +1,17 @@
+package com.example.notetakingapp.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey
+    val id: Int,
+    val notyTitle: String,
+    val noteBody: String
+
+): Parcelable
