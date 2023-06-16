11073fee6d5b2d6c3307d251239dcb8c988aafab
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-16 12:55:07 +0100

    Code updated on the note adapter

--- New Android/NoteTakingApp/app/src/main/java/com/example/notetakingapp/adapter/NoteAdapter.kt
+++ New Android/NoteTakingApp/app/src/main/java/com/example/notetakingapp/adapter/NoteAdapter.kt
@@ -1,19 +1,22 @@
 package com.example.notetakingapp.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.notetakingapp.databinding.NoteLayoutBinding
+import com.example.notetakingapp.fragments.HomeFragment
 import com.example.notetakingapp.model.Note
+import kotlin.random.Random
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
 
-        class NoteViewHolder(val itemBinding: NoteLayoutBinding) :
-            RecyclerView.ViewHolder(itemBinding.root)
+    class NoteViewHolder(val itemBinding: NoteLayoutBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
 
-    private  val differCallback = object: DiffUtil.ItemCallback<Note>() {
+    private val differCallback = object : DiffUtil.ItemCallback<Note>() {
         override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
             return oldItem.id == newItem.id &&
                     oldItem.noteBody == newItem.noteBody &&
@@ -34,6 +37,31 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
                 LayoutInflater.from(parent.context), parent, false
             )
         )
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote= differ.currentList[position]
+
+        holder.itemBinding.tvNoteTitle.text = currentNote.noteTitle
+        holder.itemBinding.tvNoteBody.text = currentNote.noteBody
+
+        val random = Random()
+        val color = Color.argb(
+            255,
+            random.nextInt(256),
+            random.nextInt(256),
+            random.nextInt(256)
+        )
+
+        holder.itemBinding.ibColor.setBackgroundColor(color)
+
+        holder.itemView.setOnClickListener{
+            val direction = HomeFragmentDirections.action()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
     }
 
 }
