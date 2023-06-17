bb31912cb9a75d2789e8f874a4c43df253df3ecc
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-17 07:38:00 +0100

    Code updated in Update Note Fragment.

--- New Android/NoteTakingApp/app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
+++ New Android/NoteTakingApp/app/src/main/java/com/example/notetakingapp/fragments/UpdateNoteFragment.kt
@@ -1,8 +1,12 @@
 package com.example.notetakingapp.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -74,11 +78,42 @@ class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
                     Toast.LENGTH_LONG).show()
             }
 
+            deleteNote()
+
         }
     }
 
     private fun deleteNote() {
+        AlertDialog.Builder(activity).apply {
+            setTitle("Delete Note")
+            setMessage("Are you sure you want to delete this note")
+            setPositiveButton("Delete") { _, _ ->
+                notesViewModel.deleteNote(currentNote)
 
+                view?.findNavController()?.navigate(R.id.action_updateNoteFragment_to_homeFragment)
+            }
+            setNegativeButton("Cancel", null)
+        }.create().show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_update_note, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_delete -> {
+                deleteNote()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
