75e4deb0a03d89f80dd4e4444bfcd43c08b28949
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-19 18:55:41 +0100

    Code added to layout file.

---
+++ New Android/CoroutinesApp/app/src/main/java/com/example/coroutinesapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.coroutinesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.coroutinesapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var  counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+}
