b74522eff553e4784b00ed2a17946c52d2a5c6fc
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-06-21 09:55:47 +0100

    Layout and MainActivity.kt code updated.

--- New Android/CoroutinesApp/app/src/main/java/com/example/coroutinesapp/MainActivity.kt
+++ New Android/CoroutinesApp/app/src/main/java/com/example/coroutinesapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.coroutinesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.coroutinesapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,9 +36,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun downloadBigFile() {
+    private suspend fun downloadBigFile() {
         for (i in 1..100000) {
-            Log.i("TAG", "Downloading $i on ${Thread.currentThread().name}")
+            //Log.i("TAG", "Downloading $i on ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                binding.textView2.text = "$i in ${Thread.currentThread().name}"
+            }
         }
     }
 
