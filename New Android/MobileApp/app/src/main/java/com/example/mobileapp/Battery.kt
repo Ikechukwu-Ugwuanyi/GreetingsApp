3f048d7a12c8b82a6250f1e591ea4c7e80187f92
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-07-04 18:18:03 +0100

    Classes created!

---
+++ New Android/MobileApp/app/src/main/java/com/example/mobileapp/Battery.kt
@@ -0,0 +1,14 @@
+package com.example.mobileapp
+
+import android.util.Log
+
+class Battery {
+
+    init {
+        Log.i("TAG", "Battery Created")
+    }
+
+    fun displayBatterPower() {
+        Log.i("TAG", "Battery is 100%")
+    }
+}
