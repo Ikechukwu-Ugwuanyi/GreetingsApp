e4f4160863d16fbdc7ac04a308597f14e7bab84e
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-07-08 00:02:02 +0100

    MainActivity.kt code updated!

--- New Android/RetrofitApp/app/src/main/java/com/example/retrofitapp/AlbumItem.kt
+++ New Android/RetrofitApp/app/src/main/java/com/example/retrofitapp/AlbumItem.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class AlbumItem (
     @SerializedName("id")
     val id: Int,
+
     @SerializedName("userId")
     val userId: Int,
     @SerializedName("title")
