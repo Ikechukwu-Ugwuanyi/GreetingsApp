7766b0cafe434e335a74a1df2272b25d4364aa72
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-06 21:23:21 +0100

    Files Updated

---
+++ New Android/ContactsManagerApp/app/src/main/java/com/example/contactsmanagerapp/room/Database.kt
@@ -0,0 +1,33 @@
+package com.example.contactsmanagerapp.room
+
+import android.content.Context
+import android.provider.CalendarContract.Instances
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1)
+abstract class UserDatabase : RoomDatabase() {
+
+    abstract val userDAO : UserDAO
+
+    //singleton patter
+    companion object{
+        @Volatile
+        private  var INSTANCE:UserDatabase ?= null
+        fun getInstance(context: Context) :UserDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDatabase::class.java,
+                        "user_db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+
+    }
+}
