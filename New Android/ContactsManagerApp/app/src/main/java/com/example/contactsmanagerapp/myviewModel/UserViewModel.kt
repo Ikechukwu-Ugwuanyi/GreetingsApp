ad57b044251c4840f4b7f9c93eb325588db2447b
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-07 15:21:44 +0100

    UserViewModel class created and Code Added.

---
+++ New Android/ContactsManagerApp/app/src/main/java/com/example/contactsmanagerapp/myviewModel/UserViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.contactsmanagerapp.myviewModel
+
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.contactsmanagerapp.room.User
+import com.example.contactsmanagerapp.room.UserRepository
+
+class UserViewModel(private val repository: UserRepository) :ViewModel(), Observable {
+
+    val users = repository.users
+    var isUpdateOrDelete = false
+    private lateinit var userToUpdateOrDelete: User
+
+    @Bindable
+    val  inputName = MutableLiveData<String>()
+
+    @Bindable
+    val inputEmail = MutableLiveData<String>()
+
+    @Bindable
+    val saveOrDeleteButtonText = MutableLiveData<String>()
+
+    @Bindable
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+
+
+
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
