40496d702c77d2e3029a95d32ddaaf1e508f6558
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-13 13:43:05 +0100

    Code Added to UserAdapter.kt

---
+++ New Android/ContactsManagerPractice/app/src/main/java/com/example/contactsmanagerpractice/Main/viewUI/UserAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.contactsmanagerpractice.Main.viewUI
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contactsmanagerpractice.Main.model.User
+import com.example.contactsmanagerpractice.R
+import com.example.contactsmanagerpractice.databinding.CardItemBinding
+
+class UserAdapter(private val userList: List<User>, private val clickListener :  (User) -> Unit) :
+    RecyclerView.Adapter<UserAdapter.MyViewHolder>() {
+
+        class MyViewHolder(private val binding: CardItemBinding) : RecyclerView.ViewHolder(binding.root){
+            fun bind(user: User, clickListener: (User) -> Unit) {
+                binding.nameTxt.text = user.name
+                binding.nameEmail.text = user.email
+
+                binding.listItem.setOnClickListener {
+                    clickListener(user)
+                }
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layout = LayoutInflater.from(parent.context)
+        val binding : CardItemBinding =
+            DataBindingUtil.inflate(layout, R.layout.card_item, parent, false)
+        return MyViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(userList[position], clickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
