a5bb9cb1fac049ff98775ab8dd666e9d4a271d11
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-13 15:11:49 +0100

    Code updated in card_item.xml and MainActivity.kt

--- New Android/ContactsManagerPractice/app/src/main/java/com/example/contactsmanagerpractice/Main/myViewModel/MainActivity.kt
+++ New Android/ContactsManagerPractice/app/src/main/java/com/example/contactsmanagerpractice/Main/myViewModel/MainActivity.kt
@@ -2,15 +2,21 @@ package com.example.contactsmanagerpractice.Main.myViewModel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.contactsmanagerpractice.Main.model.User
 import com.example.contactsmanagerpractice.Main.model.UserDb
 import com.example.contactsmanagerpractice.Main.model.UserRepository
+import com.example.contactsmanagerpractice.Main.viewUI.UserAdapter
 import com.example.contactsmanagerpractice.R
 import com.example.contactsmanagerpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var userViewModel: UserViewModel
+    //private  lateinit var adapter :UserAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +36,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
-        TODO("Not yet implemented")
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        displayUsersList()
+    }
+
+    private fun displayUsersList() {
+        userViewModel.users.observe(this, Observer {
+            binding.recyclerView.adapter = UserAdapter(it) {
+                selectedItem : User -> listItemClicked(selectedItem)
+            }
+        })
+    }
+
+    private fun listItemClicked(selectedItem: User) {
+        Toast.makeText(this, "The seleceted Item is ${selectedItem.name}", Toast.LENGTH_LONG).show()
+        binding.userViewModel.initUpdateOrDelete(selectedItem)
     }
 
 
