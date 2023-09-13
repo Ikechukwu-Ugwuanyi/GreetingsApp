f1baa27499240d54ffbbbb98150bf90a44150992
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-13 08:00:49 +0100

    User Database updated with code!

---
+++ New Android/ContactsManagerPractice/app/src/main/java/com/example/contactsmanagerpractice/myviewModel/model/UserDb.kt
@@ -0,0 +1,32 @@
+package com.example.contactsmanagerpractice.myviewModel.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [User::class], version = 1)
+abstract class UserDb : RoomDatabase() {
+
+    abstract val userDAO : UserDAO
+
+    companion object {
+        @Volatile
+        var INSTANCE: UserDb ?= null
+        fun getInstance(context: Context) : UserDb {
+            synchronized(this) {
+                var instance = INSTANCE
+                if(instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDb::class.java,
+                        "User_db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+
+}
