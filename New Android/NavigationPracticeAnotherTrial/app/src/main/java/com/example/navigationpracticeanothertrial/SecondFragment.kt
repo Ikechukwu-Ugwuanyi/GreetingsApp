ad5891b20ea9fb233a34519cf28bb55676bf38e0
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-16 21:55:38 +0100

    Code updated in Files

---
+++ New Android/NavigationPracticeAnotherTrial/app/src/main/java/com/example/navigationpracticeanothertrial/SecondFragment.kt
@@ -0,0 +1,29 @@
+package com.example.navigationpractice
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navigationpracticeanothertrial.R
+import com.example.navigationpracticeanothertrial.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+
+        val args = arguments?.getString("name")
+        binding.secondFrag.text = args
+
+        return binding.root
+    }
+
+}
