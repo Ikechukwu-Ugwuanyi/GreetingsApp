7ca7042e4d06f5f60aa01790cb198f4bdf5498ec
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-18 16:22:06 +0100

    NoteDatabase updated, Repository created.

--- New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/database/NoteDatabase.kt
+++ New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/database/NoteDatabase.kt
@@ -2,6 +2,7 @@ package com.example.notetakinapp2.database
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.notetakinapp2.model.Note
 
@@ -17,7 +18,18 @@ abstract class NoteDatabase : RoomDatabase() {
 
         operator fun invoke(context: Context) = instance ?:
         synchronized(LOCK) {
-
+            instance ?:
+            createDatabase(context).also{
+                instance = it
+            }
         }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                NoteDatabase::class.java,
+                "note_db"
+            ).build()
     }
+
 }
