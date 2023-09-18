3ab1c68d9639b2fd11d22446c4c2ff7a119fd030
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-18 13:50:57 +0100

    Model class and Database created.

--- New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/MainActivity.kt
+++ New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.notetakinapp2
+package com.example.notetakinapp2.fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.notetakinapp2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
