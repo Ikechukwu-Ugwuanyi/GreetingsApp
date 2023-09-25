a76326a32f52d91d7b986585d44c6e96ee29a9df
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-26 00:21:14 +0100

    NewNoteFragment.kt code updated, New note fragment layout modified!

--- New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/NewNoteFragment.kt
+++ New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/NewNoteFragment.kt
@@ -2,12 +2,18 @@ package com.example.notetakinapp2.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.notetakinapp2.R
 import com.example.notetakinapp2.adapter.NoteAdapter
 import com.example.notetakinapp2.databinding.FragmentNewNoteBinding
+import com.example.notetakinapp2.model.Note
 import com.example.notetakinapp2.viewModel.NoteViewModel
 
 
@@ -18,6 +24,7 @@ class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
 
     private lateinit var notesViewModel : NoteViewModel
     private lateinit var noteAdapter : NoteAdapter
+    private lateinit var mView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +36,48 @@ class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_note, container, false)
+        _binding = FragmentNewNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        notesViewModel = (activity as MainActivity).noteViewModel
+        mView = view
+    }
+
+    private fun saveNote(view: View) {
+        val noteTitle = binding.editNoteTitle.text.toString().trim()
+        val noteBody = binding.editNoteBody.text.toString().trim()
+
+        if(noteTitle.isNotEmpty()) {
+            val note = Note(0, noteTitle, noteBody)
+
+            notesViewModel.addNote(note)
+
+            Toast.makeText(this, "Note Saved Successfully", Toast.LENGTH_LONG).show()
+
+            view.findNavController().navigate(R.id.action_newNoteFragment_to_homeFragment)
+
+        } else {
+            Toast.makeText(this, "Please enter note title", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        menu.clear()
+        inflater.inflate(R.menu.home_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+
+        val mMenuSearch = menu.findItem(R.id.menu_search).actionView as SearchView
+        mMenuSearch.isSubmitButtonEnabled = false
+        mMenuSearch.setOnQueryTextListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
