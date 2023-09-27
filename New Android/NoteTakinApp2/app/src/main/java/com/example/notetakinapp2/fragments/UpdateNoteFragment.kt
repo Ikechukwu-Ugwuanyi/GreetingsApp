c9d470b7020673e81fe9127292602aee4dd7f13f
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-27 17:43:52 +0100

    Code Updated in UpdateNoteFragment.kt.

--- New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/UpdateNoteFragment.kt
+++ New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/UpdateNoteFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.notetakinapp2.R
 import com.example.notetakinapp2.databinding.FragmentUpdateNoteBinding
@@ -40,7 +42,27 @@ class UpdateNoteFragment : Fragment(R.layout.fragment_update_note) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         notesViewModel = (activity as MainActivity).noteViewModel
-        currentNote = args.note!!
+        currentNote = args.note
+
+        binding.editNoteBodyUpdate.setText(currentNote.noteBody)
+        binding.editNoteTitleUpdate.setText(currentNote.noteTitle)
+
+        //if user updates note
+        binding.floatingActionDone.setOnClickListener {
+            val title = binding.editNoteTitleUpdate.text.toString().trim()
+            val body = binding.editNoteBodyUpdate.text.toString().trim()
+
+            if(title.isNotEmpty()) {
+                val note = Note(currentNote.id, title, body)
+                notesViewModel.updateNote(note)
+                it.findNavController().navigate(R.id.action_updateNoteFragment_to_homeFragment)
+            } else {
+                Toast.makeText(context,
+                    "Please enter note Title",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 
 }
