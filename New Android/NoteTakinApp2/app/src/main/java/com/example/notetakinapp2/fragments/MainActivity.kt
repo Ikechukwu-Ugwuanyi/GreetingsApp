e036c647e3d00435050c1b85d6afdc60087ecd32
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-09-28 16:30:30 +0100

    Main Activity updated with ViewModel code.

--- New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/MainActivity.kt
+++ New Android/NoteTakinApp2/app/src/main/java/com/example/notetakinapp2/fragments/MainActivity.kt
@@ -2,15 +2,32 @@ package com.example.notetakinapp2.fragments
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.notetakinapp2.R
+import androidx.lifecycle.ViewModelProvider
+import com.example.notetakinapp2.database.NoteDatabase
+import com.example.notetakinapp2.databinding.ActivityMainBinding
+import com.example.notetakinapp2.repository.NoteRepository
 import com.example.notetakinapp2.viewModel.NoteViewModel
+import com.example.notetakinapp2.viewModel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+
+        val viewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+
     }
 }
