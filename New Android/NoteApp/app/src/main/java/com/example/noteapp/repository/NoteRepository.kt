ec9bac9d35af59d96c16652277d9109812a91d07
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-10-07 09:46:24 +0100

    NoteRepository class created!

---
+++ New Android/NoteApp/app/src/main/java/com/example/noteapp/repository/NoteRepository.kt
@@ -0,0 +1,14 @@
+package com.example.noteapp.repository
+
+import com.example.noteapp.model.Note
+import com.example.noteapp.model.NoteDatabase
+
+class NoteRepository(private val db: NoteDatabase) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDAO().insertNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDAO().insertNote(note)
+    suspend fun deleteNote(note: Note) = db.getNoteDAO().insertNote(note)
+
+    fun getAllNote() = db.getNoteDAO().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDAO().searchNote(query)
+}
