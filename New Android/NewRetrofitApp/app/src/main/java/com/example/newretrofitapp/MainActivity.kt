40a5f3b1ca15f72f6a46a445b6d1a0c2a3144f28
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-10-12 11:44:22 +0100

    Code added to the MainActivity

--- New Android/NewRetrofitApp/app/src/main/java/com/example/newretrofitapp/MainActivity.kt
+++ New Android/NewRetrofitApp/app/src/main/java/com/example/newretrofitapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.newretrofitapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofitService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+
+        val responseLiveData : LiveData<Response<Albums>> =
+            liveData {
+                val response = retrofitService.getAlbums()
+                emit(response)
+            }
+
+        responseLiveData.observe(this, Observer {
+            val albumList = it.body()?.listIterator()
+
+            if(albumList != null) {
+                while (albumList.hasNext()) {
+                    val albumItem = albumList.next()
+                    Log.i("TAG", albumItem.title)
+                }
+            }
+        })
+
     }
 }
