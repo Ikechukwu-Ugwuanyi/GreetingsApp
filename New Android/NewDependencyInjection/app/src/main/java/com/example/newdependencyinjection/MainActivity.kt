c0eeb8e651ef8891374e22f520a8d736d8f9072b
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-10-13 17:10:57 +0100

    MainActivity code updated.

--- New Android/NewDependencyInjection/app/src/main/java/com/example/newdependencyinjection/MainActivity.kt
+++ New Android/NewDependencyInjection/app/src/main/java/com/example/newdependencyinjection/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.newdependencyinjection
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
     private lateinit var mobile: Mobile
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        DaggerMobileComponent.create().getMobileInstance()
+        DaggerMobileComponent.create()
+            .inject(this)
+
+        mobile.turnOnMobile()
     }
 }
