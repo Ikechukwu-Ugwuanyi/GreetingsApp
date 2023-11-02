81375e8d753c661dc6ea031ee53a3f806800bf21
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-02 11:45:35 +0100

    Movie Dao created!.

---
+++ New Android/MovieApp/app/src/main/java/com/example/movieapp/data/db/MovieDAO.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp.data.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.movieapp.data.Movie
+
+@Dao
+interface MovieDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovie(movie: List<Movie>)
+
+    @Query("DELETE from popular_movie")
+    suspend fun deleteAllMovies()
+
+    @Query("SELECT * FROM popular_movie")
+    suspend fun getAllMovie() : List<Movie>
+
+}
