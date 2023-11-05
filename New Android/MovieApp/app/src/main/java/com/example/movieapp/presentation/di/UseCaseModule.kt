0cc75f18f06437961ad6b06d7ac140daa432b1ba
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-05 22:45:07 +0100

    GetMovieUseCase Dependency Injection class created.

---
+++ New Android/MovieApp/app/src/main/java/com/example/movieapp/presentation/di/UseCaseModule.kt
@@ -0,0 +1,15 @@
+package com.example.movieapp.presentation.di
+
+import com.example.movieapp.domain.repository.MovieRepository
+import com.example.movieapp.domain.usecase.GetMoviesUseCase
+import dagger.Module
+import dagger.Provides
+
+@Module
+class UseCaseModule {
+
+    @Provides
+    fun provideGetMovieUseCase(movieRepository: MovieRepository) : GetMoviesUseCase {
+        return GetMoviesUseCase(movieRepository)
+    }
+}
