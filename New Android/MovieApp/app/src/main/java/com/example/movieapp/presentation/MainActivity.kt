c8c077e86b0f65f110fe597d6839f4e90c19b8a3
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-07 15:58:10 +0100

    MainActivity code updated, Update menu resource file created.

--- New Android/MovieApp/app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
+++ New Android/MovieApp/app/src/main/java/com/example/movieapp/presentation/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.movieapp.presentation
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -57,5 +60,41 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "There is no data available", Toast.LENGTH_LONG).show()
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    fun updateMovies(){
+        binding.progressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovie()
+
+                response.observe(this, Observer{
+                    if(it != null) {
+                        adapter.setList(it)
+                        adapter.notifyDataSetChanged()
+                        binding.progressBar.visibility = View.GONE
+                    }else {
+                        binding.progressBar.visibility = View.GONE
+                    }
+        }
+
+        )
     }
 }
