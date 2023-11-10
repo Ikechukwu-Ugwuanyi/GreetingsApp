3a4783662b6083466dfdad431f4d4579c9c07987
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-10 17:09:29 +0100

    RetrofitInstance class created.

--- New Android/QuizApp/app/src/main/java/com/example/quizapp/retrofit/RetrofitInstance.kt
+++ New Android/QuizApp/app/src/main/java/com/example/quizapp/retrofit/RetrofitInstance.kt
@@ -1,4 +1,15 @@
 package com.example.quizapp.retrofit
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitInstance {
+
+    val baseUrl = "http://192.168.0.149/quiz/"
+
+    fun retrofitInstance() : Retrofit {
+        return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
