4b280f794c177884216bcd2ac90a90c5bab3d508
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-17 19:30:15 +0100

    Result Activity updated with code!

--- New Android/QuizApp/app/src/main/java/com/example/quizapp/view/ResultActivity.kt
+++ New Android/QuizApp/app/src/main/java/com/example/quizapp/view/ResultActivity.kt
@@ -1,12 +1,24 @@
 package com.example.quizapp.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.quizapp.R
+import com.example.quizapp.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_result)
+
+        binding.textAnswer.text = "Your score is: " +MainActivity.result +"/" + MainActivity.totalQuestions
+
+        binding.btnBack.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
