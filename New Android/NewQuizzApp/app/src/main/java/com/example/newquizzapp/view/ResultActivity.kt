9c70462fad3461e17ffe7c6fdb74289f77ba8bf0
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-21 16:44:09 +0100

    activity_result.xml created for ResultActivity AndroidManifest.xml updated for ResultActivity created Code added to ResultActivity.kt class

---
+++ New Android/NewQuizzApp/app/src/main/java/com/example/newquizzapp/view/ResultActivity.kt
@@ -0,0 +1,29 @@
+package com.example.newquizzapp.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.newquizzapp.R
+import com.example.newquizzapp.databinding.ActivityResultBinding
+
+
+class ResultActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_result)
+
+        binding.scoreText.text = "Your score is: "+MainActivity.result +"/"+MainActivity.totalQuestion
+
+        binding.btnBack.setOnClickListener {
+            var  intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
