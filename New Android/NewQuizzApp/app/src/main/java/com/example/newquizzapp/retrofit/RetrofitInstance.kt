d9717428e39f6e463524a08c456a78ef34e23d24
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-21 15:41:28 +0100

    Functionality added to NEXT button in MainActivity. RetrofitInstance.kt class updated.

--- New Android/NewQuizzApp/app/src/main/java/com/example/newquizzapp/retrofit/RetrofitInstance.kt
+++ New Android/NewQuizzApp/app/src/main/java/com/example/newquizzapp/retrofit/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
 
-    val baseUrl = "http://192.168.0.149/quiz"
+    val baseUrl = "http://192.168.0.101/quiz"
 
     fun getRetrofitInstance() :  Retrofit{
         return Retrofit.Builder()
--- New Android/NewQuizzApp/app/src/main/java/com/example/newquizzapp/view/MainActivity.kt
+++ New Android/NewQuizzApp/app/src/main/java/com/example/newquizzapp/view/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.newquizzapp.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -52,6 +54,41 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             })
+        }
+
+        //Displaying the rest of the questions
+        var i = 1
+        binding.apply {
+            nextButton.setOnClickListener(View.OnClickListener {
+                val selectedButton = radioGroup.checkedRadioButtonId
+                if (selectedButton != -1){
+                    val radButton = findViewById<View>(selectedButton) as RadioButton
+
+                    questionsList.let{
+                        if(i < it.size){
+
+                            //Getting the number of questions
+                            totalQuestion = it.size
+                        }
+
+                        //Check if it is correct or not
+                        if (radButton.text.toString().equals(it[i-1].correct_option)){
+                            result++
+                            correctText.text = "Correct Answer $result"
+                        }
+
+                        //Displaying the next question
+                        questionText.text = "Question ${i+1}: "+ it[i].question
+                        radio1.text = it[i].option1
+                        radio2.text = it[i].option2
+                        radio3.text = it[i].option3
+                        radio4.text = it[i].option4
+
+                        //
+                    }
+                }
+            })
+
         }
 
     }
