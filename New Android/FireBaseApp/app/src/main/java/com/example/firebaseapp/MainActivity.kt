d4f7dfef90f155a4461c999b05730ecbe71bda59
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-24 13:49:22 +0100

    User.kt data class created. Writing object to Firebase based on User.kt model class updated in MainActivity

--- New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.firebaseapp
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
@@ -17,30 +13,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.textView)
+        //val textView: TextView = findViewById(R.id.textView)
 
         //Real Time database reference
         //https://fir-kotlin-5308f-default-rtdb.firebaseio.com/
         database = Firebase.database.reference
 
+        val user1 = User("John", "2345")
+
+        database.child("Users").setValue(user1)
+
         //Write data to firebase
-        database.child("price").setValue("$14980")
+        //database.child("price").setValue("$14980")
 
         //Reading from firebase
-        val postListener = object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val gold_price = snapshot.value
-
-                textView.text = gold_price.toString()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        }
-
-        database.child("price").addValueEventListener(postListener)
+//        val postListener = object: ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                val gold_price = snapshot.value
+//
+//                textView.text = gold_price.toString()
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        }
+//
+//        database.child("price").addValueEventListener(postListener)
 
     }
 }
