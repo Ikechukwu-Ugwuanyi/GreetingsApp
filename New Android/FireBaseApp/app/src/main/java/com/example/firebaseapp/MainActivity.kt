5f7595c15b2207260fc20f7ec7a0f8417573b095
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-24 13:33:34 +0100

    activity_main.xml updated with id added to the textView. Reading from database function added to MainActivity.

--- New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.firebaseapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +17,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView: TextView = findViewById(R.id.textView)
+
         //Real Time database reference
         //https://fir-kotlin-5308f-default-rtdb.firebaseio.com/
         database = Firebase.database.reference
 
         //Write data to firebase
-        database.child("price").setValue("$1890")
+        database.child("price").setValue("$14980")
+
+        //Reading from firebase
+        val postListener = object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val gold_price = snapshot.value
+
+                textView.text = gold_price.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        database.child("price").addValueEventListener(postListener)
 
     }
 }
