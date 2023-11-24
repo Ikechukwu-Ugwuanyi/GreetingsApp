bb448b66e017f7f0fadaf756555dbc7a0321f031
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-24 14:20:02 +0100

    User.kt Updated, MainActivity.kt updated!

--- New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.firebaseapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
+import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
@@ -13,15 +18,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val textView: TextView = findViewById(R.id.textView)
+        val textView: TextView = findViewById(R.id.textView)
 
         //Real Time database reference
         //https://fir-kotlin-5308f-default-rtdb.firebaseio.com/
         database = Firebase.database.reference
 
+        //Writing custom objects to Firebase
         val user1 = User("John", "2345")
-
         database.child("Users").setValue(user1)
+
+        //Reading custom objects from Firebase
+        val pe = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val u1 = snapshot.getValue<User>()
+                textView.text = u1.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        database.child("Users").addValueEventListener(pe)
+
 
         //Write data to firebase
         //database.child("price").setValue("$14980")
--- New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/User.kt
+++ New Android/FireBaseApp/app/src/main/java/com/example/firebaseapp/User.kt
@@ -1,3 +1,3 @@
 package com.example.firebaseapp
 
-data class User(val userName:  String, val password: String)
+data class User(val userName:  String="", val password: String="")
