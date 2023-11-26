7dcf7af387c9a5b35e2abe12da2e9d2d085bdb95
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-26 15:39:14 +0100

    activity_sign_up.xml file updated. build.gradle file changed

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.kt
@@ -2,10 +2,28 @@ package com.example.journalapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        auth = Firebase.auth
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
     }
 }
