0d5903c6822abfc80527738449710a1308bd1351
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-27 13:12:11 +0100

    Image Url in Journal data class changed to string. Data binding added to journal row layout.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/Journal.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/Journal.kt
@@ -3,7 +3,7 @@ package com.example.journalapp
 data class Journal(
     val title: String,
     val thoughts: String,
-    val imageUrl: String,
+    val imageUrl: Int,
     val userId: String,
     val timeAdded:String,
     val username:String
