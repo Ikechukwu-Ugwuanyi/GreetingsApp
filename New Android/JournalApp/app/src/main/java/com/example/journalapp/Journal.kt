6e48056ae4d66a6c85adc2411de039ead1f4c3c2
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-27 13:03:32 +0100

    More variables added to journal data class. Share icon added to drawables. Journal row layout file updated.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/Journal.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/Journal.kt
@@ -2,5 +2,9 @@ package com.example.journalapp
 
 data class Journal(
     val title: String,
-    val imageUrl: String
+    val thoughts: String,
+    val imageUrl: String,
+    val userId: String,
+    val timeAdded:String,
+    val username:String
 )
