38bf3b96441097aaacc0f1498979961f6efac709
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-27 06:42:43 +0100

    binding added to Journalist activity. layout structure added to journal_row layout.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/JournalList.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/JournalList.kt
@@ -2,10 +2,14 @@ package com.example.journalapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.journalapp.databinding.ActivityJournalListBinding
 
 class JournalList : AppCompatActivity() {
+    lateinit var binding: ActivityJournalListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_journal_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_journal_list)
     }
 }
