fa25eb7b17dcfbc6a1a0af259a7d4f524cc5f40a
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-28 07:27:21 +0100

    User credential variables and Firebase variables created.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
@@ -1,11 +1,35 @@
 package com.example.journalapp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.journalapp.databinding.ActivityAddJournalBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.StorageReference
 
 class AddJournalActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityAddJournalBinding
+    //Credentials
+    var currentUserId: String = ""
+    var currentUsername: String = ""
+
+    //Firebase
+    lateinit var auth: FirebaseAuth
+    lateinit var user:FirebaseUser
+
+    //Firestore
+    var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    lateinit var storageReference: StorageReference
+    var collectionReference: CollectionReference = db.collection("Journal")
+    lateinit var imageUri: Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_journal)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_journal)
     }
 }
