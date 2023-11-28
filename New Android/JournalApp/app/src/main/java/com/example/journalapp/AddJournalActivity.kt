379edcbb219d549c345de83213a13108f89a8368
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-28 11:54:56 +0100

    Code to verify user Login added.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
@@ -2,6 +2,7 @@ package com.example.journalapp
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.journalapp.databinding.ActivityAddJournalBinding
@@ -9,6 +10,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
 class AddJournalActivity : AppCompatActivity() {
@@ -31,5 +33,21 @@ class AddJournalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_journal)
+
+        storageReference = FirebaseStorage.getInstance().getReference()
+        auth = FirebaseAuth.getInstance()
+
+        binding.apply {
+            postProgressBar.visibility = View.INVISIBLE
+
+            //If there is a valid user logged in
+            if(JournalUser.instance != null){
+                currentUserId = JournalUser.instance!!.userId.toString()
+
+                currentUsername = JournalUser.instance!!.username.toString()
+
+                usernameTxt.text = currentUsername
+            }
+        }
     }
 }
