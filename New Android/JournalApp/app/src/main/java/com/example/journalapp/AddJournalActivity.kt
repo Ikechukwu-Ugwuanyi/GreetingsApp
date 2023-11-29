6cb1dd7b96b459f56c01bc47201ca783a92592de
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-29 05:32:33 +0100

    Click functionality added to Save button.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
@@ -2,16 +2,19 @@ package com.example.journalapp
 
 import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.journalapp.databinding.ActivityAddJournalBinding
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+
 
 class AddJournalActivity : AppCompatActivity() {
 
@@ -47,7 +50,40 @@ class AddJournalActivity : AppCompatActivity() {
                 currentUsername = JournalUser.instance!!.username.toString()
 
                 usernameTxt.text = currentUsername
+
             }
+
+            //Clicking the Save Button
+            postSaveJournalBtn.setOnClickListener(){
+                saveJournal()
+            }
+        }
+
+        //
+    }
+
+    private fun saveJournal() {
+        val title : String = binding.postTitleEt.text.toString().trim()
+        val thoughts : String = binding.postDescriptionEt.text.toString().trim()
+
+        binding.postProgressBar.visibility = View.VISIBLE
+
+        if(!TextUtils.isEmpty(title) && !TextUtils.isEmpty(thoughts) && imageUri != null){
+            //Saving the path of images in storage
+            //.../journal_images/our_image.png
+            val filePath : StorageReference = storageReference
+                .child("journal_images")
+                .child("my_image_"+ Timestamp.now().seconds)
+
+            //Uploading the images
+            filePath.putFile(imageUri)
+                .addOnSuccessListener {
+                    filePath.downloadUrl.addOnSuccessListener {
+                        var imageUri: String = it.toString()
+
+                        //Creating the object of the journal
+                    }
+                }
         }
     }
 }
