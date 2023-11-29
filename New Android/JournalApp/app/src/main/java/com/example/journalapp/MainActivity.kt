f518de6d09987b870e2dc4e02792f127f9ad60b0
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-29 06:30:40 +0100

    Firebase Auth Initialised for a currentUser.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/MainActivity.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/MainActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.journalapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    //Firebase Auth Init.
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +24,22 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //Auth Ref
+        auth = Firebase.auth
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+
+        if (currentUser != null){
+            goToJournalList()
+        }
+    }
+
+    private fun goToJournalList() {
+        var intent  = Intent(this, JournalList::class.java)
+        startActivity(intent)
     }
 }
