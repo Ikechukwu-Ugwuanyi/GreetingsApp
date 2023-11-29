a6ff3bfb3b7cae69453607d9c74174ac02a4b4ae
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2023-11-29 05:45:04 +0100

    Journal objects added to the AddJournalActivity. Journal data class updated.

--- New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
+++ New Android/JournalApp/app/src/main/java/com/example/journalapp/AddJournalActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.util.Date
 
 
 class AddJournalActivity : AppCompatActivity() {
@@ -81,7 +82,21 @@ class AddJournalActivity : AppCompatActivity() {
                     filePath.downloadUrl.addOnSuccessListener {
                         var imageUri: String = it.toString()
 
+                        var timeStamp:Timestamp = Timestamp(Date())
+
                         //Creating the object of the journal
+                        var journal : Journal = Journal(
+                            title,
+                            thoughts,
+                            imageUri,
+                            currentUserId,
+                            timeStamp,
+                            currentUsername
+                        )
+
+                        //Adding the new journal
+
+
                     }
                 }
         }
