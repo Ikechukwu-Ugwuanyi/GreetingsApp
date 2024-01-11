78d9601ba00beb33aa1f2d5a57168a21497c9bc9
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-11 15:27:27 +0100

    FontSize added to DisplayText Composable.

--- New Android/NewCompose/app/src/main/java/com/example/newcompose/MainActivity.kt
+++ New Android/NewCompose/app/src/main/java/com/example/newcompose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.newcompose.ui.theme.NewComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +47,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun DisplayOurText(){
     Text(text = stringResource(id = R.string.text_src),
-        color = Color.Blue)
+        color = Color.Blue,
+        fontSize = 30.sp)
 }
 
 @Preview(showBackground = true)
