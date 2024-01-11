eb1aad09cbae7b9dbe8ac931b7e36998e13b0c19
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-11 15:25:49 +0100

    String value added. Color added to DisplayText Composable.

--- New Android/NewCompose/app/src/main/java/com/example/newcompose/MainActivity.kt
+++ New Android/NewCompose/app/src/main/java/com/example/newcompose/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.newcompose.ui.theme.NewComposeTheme
 
@@ -43,7 +45,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun DisplayOurText(){
-    Text(text = "This is our text now.")
+    Text(text = stringResource(id = R.string.text_src),
+        color = Color.Blue)
 }
 
 @Preview(showBackground = true)
