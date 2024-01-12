83e472218d0ed2a638ce1a88bbed3352ac1c8ad1
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-12 08:51:33 +0100

    Box Layout compose created.

--- New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/BoxLayout.kt
+++ New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/BoxLayout.kt
@@ -3,18 +3,23 @@ package com.example.composecodingchallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecodingchallenge.ui.theme.ComposeCodingChallengeTheme
 
-class ColumnsAndRows : ComponentActivity() {
+class BoxLayout : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,7 +29,7 @@ class ColumnsAndRows : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting4()
+                    Greeting5()
                 }
             }
         }
@@ -32,17 +37,26 @@ class ColumnsAndRows : ComponentActivity() {
 }
 
 @Composable
-fun Greeting4() {
-    Row(horizontalArrangement = Arrangement.Center) {
+fun Greeting5() {
+    Box(modifier = Modifier.fillMaxSize()){
         Text(text = "Hello Jack")
-        Text(text = "Hello Jane")
+
+        Box(
+            Modifier.align(Alignment.TopStart)
+                .fillMaxHeight().width(50.dp)
+                .background(Color.Cyan)
+        )
+
+        Text(text = "Hello Jude", Modifier.align(Alignment.CenterStart))
     }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview4() {
+fun GreetingPreview5() {
     ComposeCodingChallengeTheme {
-        Greeting4()
+        Greeting5()
     }
 }
