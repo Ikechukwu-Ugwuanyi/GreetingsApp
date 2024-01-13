ec69413193986345f19b17bac6a7fbb0c4aa0fb6
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-13 16:03:19 +0100

    New files added for the project.

---
+++ New Android/MoreComposePractice/app/src/main/java/com/example/morecomposepractice/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.morecomposepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.morecomposepractice.ui.theme.MoreComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoreComposePracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+var count = 0
+@Composable
+fun Greeting() {
+    
+    Button(onClick = {  }) {
+        Text(text = "Click Me")
+        
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MoreComposePracticeTheme {
+        Greeting()
+    }
+}
