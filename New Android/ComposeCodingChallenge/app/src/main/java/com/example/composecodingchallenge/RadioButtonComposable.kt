07e3fdc137cdecb6ffb9d61394cd376b5ba6a1e4
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-15 12:39:01 +0100

    RadioButtonComposable composable with attributes created!

--- New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/RadioButtonComposable.kt
+++ New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/RadioButtonComposable.kt
@@ -1,14 +1,20 @@
 package com.example.composecodingchallenge
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composecodingchallenge.ui.theme.ComposeCodingChallengeTheme
 
@@ -22,7 +28,7 @@ class RadioButtonComposable : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting8("Android")
+                    Greeting8()
                 }
             }
         }
@@ -30,17 +36,21 @@ class RadioButtonComposable : ComponentActivity() {
 }
 
 @Composable
-fun Greeting8(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting8() {
+    var context = LocalContext.current
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center) {
+        RadioButton(selected = true, onClick = {
+            Toast.makeText(context, "You selected an option", Toast.LENGTH_LONG).show()
+        })
+        Text(text = "Pizza")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview8() {
     ComposeCodingChallengeTheme {
-        Greeting8("Android")
+        Greeting8()
     }
 }
