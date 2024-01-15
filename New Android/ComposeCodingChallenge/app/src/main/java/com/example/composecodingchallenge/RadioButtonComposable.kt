99b5dc560f0eb80feb1c0a4e58edde313aa4423a
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-15 13:51:02 +0100

    Radio Button Group composable created.

--- New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/RadioButtonComposable.kt
+++ New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/RadioButtonComposable.kt
@@ -5,17 +5,27 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecodingchallenge.ui.theme.ComposeCodingChallengeTheme
 
 class RadioButtonComposable : ComponentActivity() {
@@ -28,7 +38,8 @@ class RadioButtonComposable : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting8()
+                    //Greeting8()
+                    RadioGroup()
                 }
             }
         }
@@ -45,6 +56,41 @@ fun Greeting8() {
         })
         Text(text = "Pizza")
     }
+}
+
+@Composable
+fun RadioGroup(){
+    var ctx = LocalContext.current
+
+    val radioOptions = listOf("Camry", "BMW", "Toyota")
+
+    var (selectedItem, onOptionsSelected) = remember {
+        mutableStateOf(radioOptions[0])
+    }
+
+    Column(modifier = Modifier.selectableGroup()) {
+
+        radioOptions.forEach { label ->
+            Row (modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .selectable(
+                    selected = (selectedItem == label),
+                    onClick = { onOptionsSelected(label) },
+                    role = Role.RadioButton
+                )
+                .padding(horizontal = 16.dp)){
+                RadioButton(modifier = Modifier.padding(end = 16.dp),
+                    onClick = { onOptionsSelected(label) },
+                    selected = (selectedItem == label))
+                Text(text = label)
+
+            }
+        }
+
+    }
+
+
 }
 
 @Preview(showBackground = true)
