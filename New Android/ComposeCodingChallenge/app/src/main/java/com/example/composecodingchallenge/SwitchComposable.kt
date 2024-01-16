c72dba59f0aa58ad7aaa2d91c82308b74080d30d
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-16 06:40:04 +0100

    Switch Composable function created.

--- New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/SwitchComposable.kt
+++ New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/SwitchComposable.kt
@@ -3,12 +3,25 @@ package com.example.composecodingchallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecodingchallenge.ui.theme.ComposeCodingChallengeTheme
 
 class SwitchComposable : ComponentActivity() {
@@ -30,7 +43,23 @@ class SwitchComposable : ComponentActivity() {
 
 @Composable
 fun Greeting9() {
+    var switchOn by remember {
+        mutableStateOf(false)
+    }
 
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center) {
+        Switch(checked = switchOn,
+            onCheckedChange = {switchState ->
+                switchOn = switchState
+            },
+            colors = SwitchDefaults.colors(
+                checkedTrackColor = Color.Blue,
+                checkedThumbColor = Color.Gray
+            ),
+            modifier = Modifier.padding(15.dp))
+        Text(text = "Dark Mode")
+    }
 }
 
 @Preview(showBackground = true)
