2342b712aee1d28f559eb2e42e71b0a205bb4a9a
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-16 12:39:06 +0100

    Top Bar Action Composable Created.

---
+++ New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/Scafolds.kt
@@ -0,0 +1,79 @@
+package com.example.composecodingchallenge
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composecodingchallenge.ui.theme.ComposeCodingChallengeTheme
+
+class Scafolds : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCodingChallengeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    //modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting12()
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun Greeting12() {
+    var ctx = LocalContext.current.applicationContext
+    TopAppBar(title = {
+        Text(text = "Our Courses",
+            color = Color.White)
+    },
+        navigationIcon = {
+            IconButton(onClick = {
+                Toast.makeText(
+                    ctx,
+                    "You clicked Navigation",
+                    Toast.LENGTH_LONG
+                ).show()
+            }) {
+                Icon(
+                    imageVector = Icons.Outlined.Menu,
+                    contentDescription = "Menu Icon",
+
+                )
+
+            }
+        },
+         colors = TopAppBarDefaults.topAppBarColors(
+             Color.Red
+         )
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview14() {
+    ComposeCodingChallengeTheme {
+        Greeting12()
+    }
+}
