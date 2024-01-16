51a167e606e94e7fc6d056d457d805f3472be92e
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-16 08:00:28 +0100

    CheckboxGroup Composable created and customized.

--- New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/CheckBoxGroup.kt
+++ New Android/ComposeCodingChallenge/app/src/main/java/com/example/composecodingchallenge/CheckBoxGroup.kt
@@ -1,14 +1,24 @@
 package com.example.composecodingchallenge
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composecodingchallenge.ui.theme.ComposeCodingChallengeTheme
 
@@ -22,7 +32,7 @@ class CheckBoxGroup : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting10("Android")
+                    CheckGroup()
                 }
             }
         }
@@ -30,17 +40,36 @@ class CheckBoxGroup : ComponentActivity() {
 }
 
 @Composable
-fun Greeting10(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CheckGroup(){
+
+    var foods : List<String> = listOf("Rice", "Beans", "Yam")
+    var ctx = LocalContext.current.applicationContext
+
+    Column(verticalArrangement = Arrangement.Center) {
+
+        foods.forEach { check ->
+            var checkedState by remember {
+                mutableStateOf(false)
+            }
+
+            Row() {
+                Checkbox(checked = checkedState,
+                    onCheckedChange = { newCheck ->
+                        checkedState = newCheck
+                        Toast.makeText(ctx, "You clicked $check $checkedState", Toast.LENGTH_LONG).show()
+                    })
+                Text(text = check)
+            }
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview11() {
     ComposeCodingChallengeTheme {
-        Greeting10("Android")
+        CheckGroup()
     }
 }
