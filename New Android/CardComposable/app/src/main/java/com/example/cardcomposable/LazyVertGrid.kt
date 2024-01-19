1b84ce08a905fb1b97147a3a7e51672557b01ae2
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-19 14:16:14 +0100

    Card Composable in Lazy Vertical Grid customised.

--- New Android/CardComposable/app/src/main/java/com/example/cardcomposable/LazyVertGrid.kt
+++ New Android/CardComposable/app/src/main/java/com/example/cardcomposable/LazyVertGrid.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cardcomposable.ui.theme.CardComposableTheme
 
 class LazyVertGrid : ComponentActivity() {
@@ -41,8 +48,17 @@ fun OSNames(osNames: List<String>) {
         columns = GridCells.Fixed(2), 
         content = {
             items(osNames.size){
-                index -> Card {
-                Text(text = osNames[index])
+                index -> Card(modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(8.dp),
+                    colors = CardDefaults.cardColors(Color.Red)) {
+
+                Text(text = osNames[index],
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    color = Color.White
+                )
             } 
             }
         } )   
