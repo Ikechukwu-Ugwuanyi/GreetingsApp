b084b90b42331008add7cbf8a3033933ccf0b5d9
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-20 15:22:33 +0100

    Constraint Composable created, 3 Box composables also created.

---
+++ New Android/CardComposable/app/src/main/java/com/example/cardcomposable/ConstraintLayoutComposable.kt
@@ -0,0 +1,57 @@
+package com.example.cardcomposable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.cardcomposable.ui.theme.CardComposableTheme
+
+class ConstraintLayoutComposable : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CardComposableTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ConstraintScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun  ConstraintScreen(){
+    ConstraintLayout {
+        val(yellowBox, redBox,greenBox) = createRefs()
+
+        Box(modifier = Modifier.size(50.dp).background(Color.Yellow))
+
+        Box(modifier = Modifier.size(50.dp).background(Color.Yellow))
+
+        Box(modifier = Modifier.size(50.dp).background(Color.Yellow))
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview5() {
+    CardComposableTheme {
+        ConstraintScreen()
+    }
+}
