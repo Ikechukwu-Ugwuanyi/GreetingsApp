f83d1ecb476c85985917c5299f40249e2dd613c3
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-22 12:41:18 +0100

    LazyColumn Composable added to DisplayCards composable.

--- New Android/CardComposable/app/src/main/java/com/example/cardcomposable/MainActivity2.kt
+++ New Android/CardComposable/app/src/main/java/com/example/cardcomposable/MainActivity2.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cardcomposable.ui.theme.CardComposableTheme
 
 class MainActivity2 : ComponentActivity() {
@@ -49,6 +53,15 @@ fun DisplayCards() {
             "8890 2345 4322 7765",
             R.drawable.master_card,
             R.drawable.background_4))
+
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)){
+
+        items(cardList){
+                cardList -> CreditCard(cardInfo = cardList)
+        }
+    }
 
 
 }
