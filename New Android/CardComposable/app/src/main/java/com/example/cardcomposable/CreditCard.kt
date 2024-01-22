ac26182a819302bd7028733bfae792435f50ca40
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-22 13:35:06 +0100

    Card Image drawables changed. Composable functions modified in CreditCard.kt

--- New Android/CardComposable/app/src/main/java/com/example/cardcomposable/CreditCard.kt
+++ New Android/CardComposable/app/src/main/java/com/example/cardcomposable/CreditCard.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -22,37 +23,56 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun CreditCard(cardInfo: CardInfo){
-    Box{
+fun CreditCard(cardInfo: CardInfo) {
 
-        Card(
-            modifier = Modifier.height(200.dp),
-            elevation = CardDefaults.cardElevation(8.dp),
-            shape = RoundedCornerShape(8.dp)
-        ) {
-            Image(painter = painterResource(id = cardInfo.backgroundImage),
+
+    Card(
+        modifier = Modifier.height(200.dp),
+        elevation = CardDefaults.cardElevation(8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Box() {
+            Image(
+                painter = painterResource(id = cardInfo.backgroundImage),
                 contentDescription = "Background Image",
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.FillBounds,
             )
-        }
 
-        Box(modifier = Modifier.padding(16.dp)) {
-            Image(painter = painterResource(id = cardInfo.providerImage),
-                contentDescription = "Provider Logo",
+            Box(
                 modifier = Modifier
-                    .width(86.dp)
-                    .align(Alignment.TopStart))
+                    .padding(16.dp)
+                    .fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(id = cardInfo.providerImage),
+                    contentDescription = "Provider Logo",
+                    modifier = Modifier
+                        .width(86.dp)
+                        .align(Alignment.TopStart)
+                )
 
-            Column(modifier = Modifier.align(Alignment.BottomStart)) {
-                Text(text = cardInfo.cardHolder,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    letterSpacing = 1.2.sp)
-                Text(text = cardInfo.cardNumber,
-                    fontSize = 16.sp,
-                    letterSpacing = 1.1.sp)
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                ) {
+
+                    Text(
+                        text = cardInfo.cardHolder,
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        letterSpacing = 1.2.sp
+                    )
+                    Text(
+                        text = cardInfo.cardNumber,
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        letterSpacing = 1.1.sp
+                    )
+                }
             }
+
 
         }
 
@@ -63,11 +83,13 @@ fun CreditCard(cardInfo: CardInfo){
 
 @Composable
 @Preview(showBackground = true)
-fun CreditCardPreview(){
-    val cardDemo = CardInfo("1234 5678 1235 0987",
+fun CreditCardPreview() {
+    val cardDemo = CardInfo(
+        "1234 5678 1235 0987",
         "IKECHUKWU UGWUANYI",
         R.drawable.background_1,
-        R.drawable.master_card)
+        R.drawable.mastercard
+    )
 
     CreditCard(cardDemo)
 }
