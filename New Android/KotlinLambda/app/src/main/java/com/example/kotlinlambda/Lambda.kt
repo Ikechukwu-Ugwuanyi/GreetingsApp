090c76b8a1daf91e7904b703c78fd5e6684e3ba2
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-29 14:18:15 +0100

    Anonymous function added.

--- New Android/KotlinLambda/app/src/main/java/com/example/kotlinlambda/Lambda.kt
+++ New Android/KotlinLambda/app/src/main/java/com/example/kotlinlambda/Lambda.kt
@@ -1,29 +1,32 @@
 package com.example.kotlinlambda
 
-fun main(){
+fun main() {
     val welcome = { println("Welcome to our course") }
     welcome.invoke()
 
     //1. Lambda with parameters and return value
-    val add: (Int, Int) -> Int = {a:Int, b: Int -> a + b}
-    println(add(3,5))
-
+    val add: (Int, Int) -> Int = { a: Int, b: Int -> a + b }
+    println(add(3, 5))
 
     //2. Lambda with parameters and no return value
-   val add2 : (Int, Int) -> Unit = {a,b -> println(a+b) }
-    add2.invoke(8,9)
-
-
+    val add2: (Int, Int) -> Unit = { a, b -> println(a + b) }
+    add2.invoke(8, 9)
 
     //3. No parameters but with return value
-
+    val add3: () -> String = { "I am here" }
+    println(add3())
 
     //4. No parameters and no return values
-
+    val add4: () -> Unit = { println("Welcome again, here we go!") }
+    add4.invoke()
 
     //Direct use of lambda expression, with no variable
-
+    println({ a: Int, b: Int -> a + b }(4, 9))
 
     //ANONYMOUS FUNCTIONS
+    val anon = fun(a: Int, b: Int): Int {
+        return a + b
+    }
+    println(anon.invoke(9, 90))
 
 }
