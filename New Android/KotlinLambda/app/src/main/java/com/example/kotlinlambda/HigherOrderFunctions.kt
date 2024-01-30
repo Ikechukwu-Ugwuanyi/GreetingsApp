b9f2dbba466c7ad186f994496ad329d3517b0c3a
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-30 10:54:14 +0100

    "it" keyword used in file

--- New Android/KotlinLambda/app/src/main/java/com/example/kotlinlambda/HigherOrderFunctions.kt
+++ New Android/KotlinLambda/app/src/main/java/com/example/kotlinlambda/HigherOrderFunctions.kt
@@ -2,10 +2,26 @@ package com.example.kotlinlambda
 
 fun main(){
 
+
+    //LAMBDA NOTES: There are two cases where parenthesis can be omitted from Lambdas.
+    // Case 1: If the lambda is the only argument, you can omit the parenthesis
     //val add = {a:Int, b:Int -> a + b}
     hof{a:Int, b:Int -> a + b}
 
+    //Case 2: If the lambda is the last argument
     hof2("Ikechukwu") { c: Int, b: Int -> c + b }
+
+    hof3 {
+        println("Hi $it")
+    }
+
+    val numbers = listOf(9,2,3,8,2,1,0,7,5)
+
+    println(numbers.filter { numbers -> numbers > 5 })
+
+    println(numbers.filter {
+        it > 5
+    })
 }
 
 //Higher Order Function
@@ -18,4 +34,8 @@ fun hof(addition:(Int, Int) -> Int){
 fun hof2(name: String, addition: (Int, Int) -> Int){
     val result = addition(20,8)
     println("Hello $name $result")
+}
+
+fun hof3(name : (String) -> Unit){
+    name("John")
 }
