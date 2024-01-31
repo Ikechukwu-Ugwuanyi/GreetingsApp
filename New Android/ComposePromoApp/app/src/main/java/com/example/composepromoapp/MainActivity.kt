d73e7fc4bd9a07159cd8da04b612fb7d9884684c
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-31 12:35:04 +0100

    HomeAppBar Composable created!

--- New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/MainActivity.kt
+++ New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/MainActivity.kt
@@ -3,17 +3,24 @@ package com.example.composepromoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composepromoapp.ui.theme.ComposePromoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,6 +59,28 @@ fun HomeScreen(onAboutClick: () -> Unit, onDetailsClick: (title: String) -> Unit
                     onclick = { onDetailsClick(item.title) })
             }
         }
+    }
+}
+
+@Composable
+private fun HomeAppBar(onAboutClick: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp)
+    ) {
+
+        Text(
+            text = "My Udemy Courses",
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        TextButton(onClick = { onAboutClick }) {
+            Text(
+                text = "About",
+                fontSize = 24.sp
+            )
+        }
+
     }
 }
 
