ae7e6846b5b9cad83630ae0308112584ea5560ad
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-01-31 10:38:32 +0100

    A regular function returned from Higher order function(hof6)

--- New Android/KotlinLambda/app/src/main/java/com/example/kotlinlambda/HigherOrderFunctions.kt
+++ New Android/KotlinLambda/app/src/main/java/com/example/kotlinlambda/HigherOrderFunctions.kt
@@ -1,11 +1,11 @@
 package com.example.kotlinlambda
 
-fun main(){
+fun main() {
 
     //LAMBDA NOTES: There are two cases where parenthesis can be omitted from Lambdas.
     // Case 1: If the lambda is the only argument, you can omit the parenthesis
     //val add = {a:Int, b:Int -> a + b}
-    hof{a:Int, b:Int -> a + b}
+    hof { a: Int, b: Int -> a + b }
 
     //Case 2: If the lambda is the last argument
     hof2("Chukchi") { c: Int, b: Int -> c + b }
@@ -14,7 +14,7 @@ fun main(){
         println("Hi $it")
     }
 
-    val numbers = listOf(9,2,3,8,2,1,0,7,5)
+    val numbers = listOf(9, 2, 3, 8, 2, 1, 0, 7, 5)
 
     println(numbers.filter { numbers -> numbers > 5 })
 
@@ -26,7 +26,7 @@ fun main(){
     reverseString("Hello") { it.reversed() }
 
     //Passing an anonymous function as a Parameter to HOF
-    val mySub = fun(a:Int, b:Int) : Int{
+    val mySub = fun(a: Int, b: Int): Int {
         return a - b
     }
 
@@ -35,39 +35,51 @@ fun main(){
     //Returning a value from an anonymous function.
     println(hof5()())
 
+    //Returning a regular function from HOF
+    println(hof6()())
 
+
+    //Passing a regular function to HOFA
 
 }
 
 //Higher Order Function
 
-fun hof(addition:(Int, Int) -> Int){
-    val result = addition(6,10)
+fun hof(addition: (Int, Int) -> Int) {
+    val result = addition(6, 10)
     println(result)
 }
 
-fun hof2(name: String, addition: (Int, Int) -> Int){
-    val result = addition(20,8)
+fun hof2(name: String, addition: (Int, Int) -> Int) {
+    val result = addition(20, 8)
     println("Hello $name $result")
 }
 
-fun hof3(name : (String) -> Unit){
+fun hof3(name: (String) -> Unit) {
     name("John")
 }
 
-fun reverseString(str : String, myLambda: (String) -> (String)){
+fun reverseString(str: String, myLambda: (String) -> (String)) {
     val result = myLambda(str)
     println(result)
 }
 
-fun hof4(subtract: (Int, Int) -> Int){
+fun hof4(subtract: (Int, Int) -> Int) {
     val result = subtract(30, 7)
     println(result)
 }
 
-fun hof5() : () -> String {
-    val msg = fun() : String {
-        return  "Hello from HOF5"
+fun hof5(): () -> String {
+    val msg = fun(): String {
+        return "Hello from HOF5"
     }
     return msg
+}
+
+fun hof6(): () -> String {
+    return :: message
+}
+
+fun message(): String {
+    return "Hi from HOF 6"
 }
