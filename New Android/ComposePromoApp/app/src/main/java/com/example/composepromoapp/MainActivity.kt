dcf5bc714745e0c62869de5e942111352b6c0442
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-02 12:02:49 +0100

    DetailsScreen composable updated.

--- New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/MainActivity.kt
+++ New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.ArrowBack
@@ -35,6 +37,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -280,10 +283,16 @@ fun DetailsScreen(title: String, onNavigateUp: () -> Unit) {
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
-                Text(text = title, fontSize = 40.sp)
                 Text(
-                    text = chosen_course.body, Modifier.fillMaxSize(),
+                    text = title, fontWeight = FontWeight.Bold,
                     fontSize = 20.sp
+                )
+                Text(
+                    text = chosen_course.body,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(state = rememberScrollState()),
+                    fontSize = 16.sp
                 )
 
             }
