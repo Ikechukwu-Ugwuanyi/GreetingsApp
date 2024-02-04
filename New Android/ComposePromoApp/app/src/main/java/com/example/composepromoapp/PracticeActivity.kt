8baeb153cf5da223c7a46d2cd3362d0b8e77e0ea
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-04 23:59:29 +0100

    Preview Deleted

--- New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
+++ New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
@@ -3,13 +3,8 @@ package com.example.composepromoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composepromoapp.ui.theme.ComposePromoAppTheme
 
 class PracticeActivity : ComponentActivity() {
@@ -18,12 +13,7 @@ class PracticeActivity : ComponentActivity() {
         setContent {
             ComposePromoAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
@@ -31,16 +21,5 @@ class PracticeActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    ComposePromoAppTheme {
-        Greeting("Android")
-    }
 }
