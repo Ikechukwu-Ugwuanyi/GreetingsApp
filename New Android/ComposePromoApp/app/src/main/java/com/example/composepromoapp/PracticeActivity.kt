165ee76814be741f48836aedada65291f2ae53ac
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-06 23:48:50 +0100

    HomeScreen Composable created!

--- New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
+++ New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
@@ -3,6 +3,15 @@ package com.example.composepromoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.composepromoapp.ui.theme.ComposePromoAppTheme
 
 class PracticeActivity : ComponentActivity() {
@@ -15,4 +24,28 @@ class PracticeActivity : ComponentActivity() {
             }
         }
     }
+}
+
+//Home Screen
+@Composable
+fun HomeScreen2(onAboutClick: () -> Unit, onDetailsClick: (title: String) -> Unit) {
+
+    Scaffold { padding ->
+        LazyColumn(contentPadding = padding) {
+            item {
+                HomeAppBar(onAboutClick)
+            }
+
+            item {
+                Spacer(modifier = Modifier.width(20.dp))
+            }
+
+            items(allCourses) { item ->
+                CourseCard(
+                    item,
+                    onClick = { onDetailsClick(item.title) })
+            }
+        }
+    }
+
 }
