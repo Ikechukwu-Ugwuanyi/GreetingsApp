10a2d710ba6cb36b7be52193608e21963dab2ca7
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-06 23:58:45 +0100

    HomeAppBar composable created.

--- New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
+++ New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
@@ -3,15 +3,21 @@ package com.example.composepromoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composepromoapp.ui.theme.ComposePromoAppTheme
 
 class PracticeActivity : ComponentActivity() {
@@ -33,7 +39,7 @@ fun HomeScreen2(onAboutClick: () -> Unit, onDetailsClick: (title: String) -> Uni
     Scaffold { padding ->
         LazyColumn(contentPadding = padding) {
             item {
-                HomeAppBar(onAboutClick)
+                HomeAppBar2(onAboutClick)
             }
 
             item {
@@ -48,4 +54,22 @@ fun HomeScreen2(onAboutClick: () -> Unit, onDetailsClick: (title: String) -> Uni
         }
     }
 
+}
+
+@Composable
+fun HomeAppBar2(onAboutClick: () -> Unit){
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 16.dp, horizontal = 10.dp)) {
+
+        Text(text = "MyUdemy Course",
+            style = MaterialTheme.typography.bodyMedium)
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        TextButton(onClick = {onAboutClick }) {
+            Text(text = "About",
+                fontSize = 20.sp)
+
+        }
+    }
 }
