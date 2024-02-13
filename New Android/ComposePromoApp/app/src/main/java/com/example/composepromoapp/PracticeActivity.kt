48036f267f561cc26cfd93f47e6dcdf4e3dcc716
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-13 08:15:41 +0100

    AboutScreen2 created.

--- New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
+++ New Android/ComposePromoApp/app/src/main/java/com/example/composepromoapp/PracticeActivity.kt
@@ -15,8 +15,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -25,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -129,6 +135,43 @@ fun CourseCard2(item: Courses, onClick: () -> Unit) {
             )
         }
 
+    }
+
+}
+
+@Composable
+fun AboutScreen2(onNavigateUp: () -> Unit) {
+    Column {
+        Row(
+            modifier = Modifier
+                .padding(vertical = 12.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = { onNavigateUp }) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Go Back")
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+        ) {
+
+            Text(
+                text = "This is the collection of",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Normal
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            val udemyLink = LocalUriHandler.current
+
+            Button(onClick = { udemyLink.openUri("") }) {
+                Text(text = "Go to our Udemy Link")
+            }
+        }
     }
 
 }
