302a45c51312c3ec20021aa49b070cc02eff1901
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-14 22:13:50 +0100

    Counter composable created.

--- New Android/ComposeViewmodel/app/src/main/java/com/example/composeviewmodel/MainActivity.kt
+++ New Android/ComposeViewmodel/app/src/main/java/com/example/composeviewmodel/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.composeviewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeviewmodel.ui.theme.ComposeViewmodelTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Counter()
                 }
             }
         }
@@ -30,17 +35,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Counter() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeViewmodelTheme {
-        Greeting("Android")
+    var counter by remember {
+        mutableStateOf(0)
+    }
+
+    Column {
+        Button(onClick = {
+            counter++
+        }) {
+            Text(text = "Click Me")
+        }
+
+        Text(text = "The counter is $counter")
     }
 }
+
