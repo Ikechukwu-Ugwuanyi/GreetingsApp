3b20305fb81b9b1b77852579df0da520dfb9efc5
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-15 14:28:46 +0100

    ViewModel class created.

---
+++ New Android/ComposeUnitConverterApp/app/src/main/java/com/example/composeunitconverterapp/MyViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.composeunitconverterapp
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlin.math.roundToInt
+
+class MyViewModel : ViewModel() {
+
+    var tempC by mutableStateOf(0)
+
+    fun convertTemp(tempF : Int) {
+        tempC = ((tempF - 32) * 0.5556).roundToInt()
+    }
+
+}
