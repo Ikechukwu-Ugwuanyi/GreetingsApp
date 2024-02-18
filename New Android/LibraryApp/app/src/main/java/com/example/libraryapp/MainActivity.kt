52ec35efc874a74c0e79756cdb6c965cb496f58e
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-18 09:15:01 +0100

    MainScreen composable created in MainActivity

--- New Android/LibraryApp/app/src/main/java/com/example/libraryapp/MainActivity.kt
+++ New Android/LibraryApp/app/src/main/java/com/example/libraryapp/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.libraryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.libraryapp.ui.theme.LibraryAppTheme
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +38,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        var inputText by remember {
+            mutableStateOf("")
+        }
+
+        OutlinedTextField(
+            value = inputText,
+            onValueChange = { newText -> inputText = newText },
+            label = { Text(text = "Book Name") },
+            placeholder = { Text(text = "Enter Your Book Name") })
+
+        Button(onClick = { }) {
+            Text(text = "Add Book")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LibraryAppTheme {
-        Greeting("Android")
+
     }
 }
