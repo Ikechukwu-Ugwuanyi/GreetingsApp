9450cf705064e9a52f5f5e474263abb14d4a798b
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-18 08:50:41 +0100

    BookDatabase class created.

---
+++ New Android/LibraryApp/app/src/main/java/com/example/libraryapp/room/BookDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.libraryapp.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [BookEntity::class], version = 1, exportSchema = false)
+abstract class BookDatabase : RoomDatabase() {
+
+    abstract fun bookDao(): BookDAO
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: BookDatabase? = null
+
+        fun getInstance(context: Context): BookDatabase? {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        BookDatabase::class.java,
+                        "book_db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+
+    }
+}
