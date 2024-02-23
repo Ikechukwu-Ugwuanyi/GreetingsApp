5ace33f9ef11128a218798958379d63bdc6933f3
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-23 21:10:02 +0100

    Gradle  build file changed

--- New Android/LibraryApp/app/build.gradle.kts
+++ New Android/LibraryApp/app/build.gradle.kts
@@ -78,6 +78,12 @@ dependencies {
     //Annotation processor
     implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
 
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
 
