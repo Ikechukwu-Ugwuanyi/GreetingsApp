4aa25452bed140f624566359ddf188d27e1c8394
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-27 15:00:31 +0100

    BookDB created.

---
+++ New Android/LibraryAppPractice/app/src/main/java/com/example/libraryapppractice/room/BookDB.kt
@@ -0,0 +1,35 @@
+package com.example.libraryapppractice.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [BooKEntity::class], version = 1, exportSchema = false)
+abstract class BookDB : RoomDatabase() {
+
+    abstract fun bookDao(): BookDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: BookDB? = null
+
+        fun getInstance(context: Context): BookDB? {
+            synchronized(this) {
+                val instance = INSTANCE
+                if (instance == null) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        BookDB::class.java,
+                        "book_db2"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+
+        }
+
+    }
+}
