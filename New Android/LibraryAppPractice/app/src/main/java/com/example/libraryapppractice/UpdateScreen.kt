bd790a19f013af3cca6814dd6baae75a23436549
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-02-29 11:39:17 +0100

    UpdateScreen composable updated.

--- New Android/LibraryAppPractice/app/src/main/java/com/example/libraryapppractice/UpdateScreen.kt
+++ New Android/LibraryAppPractice/app/src/main/java/com/example/libraryapppractice/UpdateScreen.kt
@@ -1,6 +1,7 @@
 package com.example.libraryapppractice
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -10,6 +11,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.libraryapppractice.room.BooKEntity
 import com.example.libraryapppractice.viewmodel.BookViewModel
@@ -27,7 +30,8 @@ fun UpdateScreen(bookViewModel: BookViewModel, bookId: String){
         OutlinedTextField(value = newText,
             onValueChange = {updateText -> newText = updateText},
             label = { Text(text = "Update Text")},
-            placeholder = {Text(text = "Enter Text")})
+            placeholder = {Text(text = "Enter Text")},
+            modifier = Modifier.padding(16.dp))
 
         Button(onClick = {
             val bookRef = bookId.toInt()
