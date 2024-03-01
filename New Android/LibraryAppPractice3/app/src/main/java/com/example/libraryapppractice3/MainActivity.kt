5667c3291386cc3aac55b4e42867354c6d2f5364
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-03-01 14:12:35 +0100

    MainScreen Composable created.

--- New Android/LibraryAppPractice3/app/src/main/java/com/example/libraryapppractice3/MainActivity.kt
+++ New Android/LibraryAppPractice3/app/src/main/java/com/example/libraryapppractice3/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.libraryapppractice3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.libraryapppractice3.room.LibraryEntity
 import com.example.libraryapppractice3.ui.theme.LibraryAppPractice3Theme
+import com.example.libraryapppractice3.viewmodel.LibraryViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +37,32 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun MainScreen(viewModel: LibraryViewModel, book: LibraryEntity) {
+
+    var inputText by remember {
+        mutableStateOf("")
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = inputText,
+            onValueChange = { newText ->
+                inputText = newText },
+            placeholder = { Text(text = "Enter Book Name")},
+            label = { Text(text = "Book Name")})
+
+        Button(onClick = {
+            val add = LibraryEntity(0, inputText)
+            viewModel.addBook(add)
+        }) {
+            Text(text = "Add Book")
+
+        }
+    }
+
 }
 
 
