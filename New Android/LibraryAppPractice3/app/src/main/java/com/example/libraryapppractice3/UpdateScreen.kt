91ee21101366e16e94510fcae499e42359848d7d
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-03-01 14:40:02 +0100

    UpdateScreen Composable file/Class created.

---
+++ New Android/LibraryAppPractice3/app/src/main/java/com/example/libraryapppractice3/UpdateScreen.kt
@@ -0,0 +1,40 @@
+package com.example.libraryapppractice3
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.sp
+import com.example.libraryapppractice3.room.LibraryEntity
+import com.example.libraryapppractice3.viewmodel.LibraryViewModel
+
+@Composable
+fun UpdateScreen(viewModel: LibraryViewModel, bookId: String?){
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = "Update the Existing Book", fontSize = 24.sp)
+
+        var updateText by remember {
+            mutableStateOf("")
+        }
+
+        OutlinedTextField(value = updateText,
+            onValueChange = {newText -> updateText = newText},
+            label = { Text(text = "Enter Updsate Text")},
+            placeholder = { Text(text = "Your Text")})
+
+        Button(onClick = {
+            val bookRef = bookId!!.toInt()
+            viewModel.update(LibraryEntity(bookRef, updateText))
+        }) {
+            Text(text = "Update Book")
+        }
+    }
+}
