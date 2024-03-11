f01091e09a0e4f36f0e11142f60a086fd3e1eb9a
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-03-11 14:10:24 +0100

    Bottom Navigation bar created.

--- New Android/ComposePractices/app/src/main/java/com/example/composepractices/MainActivity.kt
+++ New Android/ComposePractices/app/src/main/java/com/example/composepractices/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.composepractices
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
@@ -11,11 +14,22 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,11 +39,12 @@ data class BottonNaigationItem(
     val title: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
-    val hasNew:Boolean,
-    val badgeCount : Int? = null
+    val hasNew: Boolean,
+    val badgeCount: Int? = null
 )
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -56,14 +71,64 @@ class MainActivity : ComponentActivity() {
                     )
                 )
                 // A surface container using the 'background' color from the theme
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
+                val content: @Composable () -> Unit
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+
                     Scaffold(
                         bottomBar = {
                             NavigationBar {
+                                items.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                        },
+                                        label = { Text(text = item.title) },
+                                        icon = {
+                                            BadgedBox(
+                                                badge = {
+                                                    if (item.badgeCount != null) {
+                                                        Badge {
+                                                            Text(text = item.badgeCount.toString())
+                                                        }
+                                                    } else if (item.hasNew) {
+                                                        Badge()
+                                                    }
+                                                }
+                                            ) {
+                                                Icon(
+                                                    imageVector = if (index == selectedItemIndex) {
+                                                        item.selectedIcon
+                                                    } else item.unselectedIcon,
+                                                    contentDescription = item.title
+                                                )
+                                            }
+                                        })
+                                }
 
+                            }
+                        },
+                        content = { paddingValues -> // Access paddingValues here
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize() // Fill the entire screen
+                                    .padding(paddingValues) // Apply the received padding
+                            ) {
+                                // Your actual screen content composables here
+                                Column {
+                                    Text("This is the content")
+                                    Button(onClick = { /* Do something */ }) {
+                                        Text("Click me")
+                                    }
+                                }
                             }
                         }
                     )
@@ -71,6 +136,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
 }
 
 
