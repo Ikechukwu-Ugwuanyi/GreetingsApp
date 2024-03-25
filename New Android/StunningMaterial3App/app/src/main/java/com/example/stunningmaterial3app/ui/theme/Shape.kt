5ceebb07455cb51dd5aee08e15b3f76d5e20fcc9
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-03-25 16:06:39 +0100

    Shape and Type updated.

---
+++ New Android/StunningMaterial3App/app/src/main/java/com/example/stunningmaterial3app/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.example.stunningmaterial3app.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+
+val shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(20.dp)
+)
