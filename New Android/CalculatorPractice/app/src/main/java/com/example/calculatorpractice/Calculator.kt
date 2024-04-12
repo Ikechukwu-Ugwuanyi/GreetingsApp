6a2fffd4ce69016385a7a9577a3b9c0d5358f89c
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-04-12 11:29:11 +0100

    CalculatorViewModel updated.

--- New Android/CalculatorPractice/app/src/main/java/com/example/calculatorpractice/Calculator.kt
+++ New Android/CalculatorPractice/app/src/main/java/com/example/calculatorpractice/Calculator.kt
@@ -216,5 +216,4 @@ fun Calculator(
             }
         }
     }
-
 }
--- New Android/CalculatorPractice/app/src/main/java/com/example/calculatorpractice/CalculatorViewModel.kt
+++ New Android/CalculatorPractice/app/src/main/java/com/example/calculatorpractice/CalculatorViewModel.kt
@@ -8,4 +8,47 @@ import androidx.lifecycle.ViewModel
 class CalculatorViewModel : ViewModel() {
     var state by mutableStateOf(CalculatorState())
         private set
+
+    fun onAction(action: CalculatorAction){
+        when(action){
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Delete -> performDeletion()
+            is CalculatorAction.Calculate -> performCalculation()
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Clear -> state = CalculatorState()
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+        }
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+
+    }
+
+//    private fun enterDecimal() {
+//        if (state.operation == null && !state.number1.contains(".")
+//            && state.number1.isNotBlank()) {
+//            state = state.copy(
+//
+//            )
+//        }
+//    }
+
+    private fun performCalculation() {
+
+    }
+
+    private fun performDeletion() {
+        when {
+            state.number2.isNotBlank() -> state = state.copy(
+
+            )
+        }
+
+    }
+
+    private fun enterNumber(number: Int) {
+
+    }
+
+
 }
