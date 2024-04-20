5720cba37d4fafea766c9998c9e246f23b7815bf
Ikechukwu Ugwuanyi <ikechukwujmartins718@gmail.com>
2024-04-20 17:09:05 +0100

    MainActivity updated.

--- New Android/CalculatorPractice3/app/src/main/java/com/example/calculatorpractice3/MainActivity.kt
+++ New Android/CalculatorPractice3/app/src/main/java/com/example/calculatorpractice3/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.calculatorpractice3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculatorpractice3.ui.theme.CalculatorPractice3Theme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +18,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalculatorPractice3Theme {
                 // A surface container using the 'background' color from the theme
+                val viewModel = viewModel<CalculatorViewModel>()
+                val buttonSpacing = 8.dp
+                val state = viewModel.state
 
-
+                Calculator(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.DarkGray)
+                        .padding(16.dp),
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                )
             }
         }
     }
